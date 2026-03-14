5629b23935e213c54ba666454704992e997e8e07
Gowtham-250803 <gowthamgc2516@gmail.com>
2026-03-14 15:13:58 +0530

    Cleaned Data
    Data cleaned using Python

---
+++ Src/data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# read dataset and skip faulty rows
+df = pd.read_csv("data/raw data/msft_stock_data_faulty_raw.csv", skiprows=2)
+
+# fix column names
+df.columns = ["Date", "Close", "High", "Low", "Open", "Volume"]
+
+print(df.head())
+
+# remove duplicates
+df = df.drop_duplicates()
+
+# remove missing prices
+df = df.dropna(subset=["Close"])
+
+# remove invalid prices
+df = df[df["Close"] > 0]
+
+# save cleaned dataset
+df.to_csv("data/cleaned data/clean_data.csv", index=False)
+
+print("Data cleaned successfully")
